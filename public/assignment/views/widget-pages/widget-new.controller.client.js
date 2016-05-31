b504c884535afc031037ad5edaf662e43c00a891
kelseeng <kelseeng@ccs.neu.edu>
2016-05-30 22:55:08 -0400

    Updated Widget-Chooser dynamically Added createwidget to widget-new controller updated widget-edit dynamically

--- public/assignment/views/widget-pages/widget-new.controller.client.js
+++ public/assignment/views/widget-pages/widget-new.controller.client.js
@@ -3,7 +3,17 @@
         .module("WebAppMaker")
         .controller("NewWidgetController", NewWidgetController)
 
-    function NewWidgetController(WidgetService) {
+    function NewWidgetController($routeParams, WidgetService) {
         var vm = this;
+        vm.userId = $routeParams.uid;
+        vm.createWidget = createWidget;
+
+        function createWidget(widgetType) {
+            var newWidget = {
+                _id:(newDate().getTime()),
+                widgetType: widgetType
+            }
+            widgets.push(newWidget);
+        }
     }
 })();
